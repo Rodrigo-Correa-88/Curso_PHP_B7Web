285e75bf0bfcfa7165694f1e52f2b0cffa147f05
Rodrigo de Souza Corrêa <correa.rodrigo88@gmail.com>
2023-08-29 15:24:11 -0300

    atualização 29/08

---
+++ MVC/introdução.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+M - MODEL 
+V - VIEW
+C - CONREOLLER
+
+O usuário requisita uma url e o MVC verifica de existe
+uma ROTA estabelecida e mandar a requisição para o 
+CONTROLLER, que por sua vez pode carregar um VIEW ou 
+carregar um MODEL.
+*/
+
